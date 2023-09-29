cfeaf54b739cfe414d0ade63cff0659bd4fa67b8
Ashley Patterson <apatterson0208@gmail.com>
2023-09-29 00:51:42 -0400

    Added all tables and set up env file

--- src/db/migrations/20230423034706_create_users_table.js
+++ src/db/migrations/20230423034706_create_users_table.js
@@ -6,6 +6,11 @@ exports.up = (knex) => knex.schema.createTable('users', (table) => {
   table.increments();
   table.string('username').notNullable().unique();
   table.string('password_hash').notNullable();
+  table.string('email').notNullable().unique();
+  table.string('first_name').notNullable();
+  table.string('last_name').notNullable();
+  table.string('bio');
+  table.string('image');
   table.timestamps(true, true);
 });
 
